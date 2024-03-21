4353f5c5ad46c4ab00b718142744147a9f30d015
ana stefanovic <anastef996@gmail.com>
2024-03-22 00:05:36 +0100

    Inicijalizacija projekta

---
+++ src/test/java/heliant/HeliantApplicationTests.java
@@ -0,0 +1,13 @@
+package heliant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
